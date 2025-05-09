1b06ffedf29ea5fbeeed8827cb6f59864148028c
austin brown <austin.brown@okta.com>
2025-05-09 14:25:42 -0700

    feat: add support for kops addons

--- apis/v1alpha1/cluster_types.go
+++ apis/v1alpha1/cluster_types.go
@@ -29,17 +29,17 @@ import (
 type ClusterParameters struct {
 
 	// State is the location of the kops state file (usually an s3 bucket)
-	State string `json:"state"`
+	State string `json:"state" yaml:"state"`
 
-	Cluster KopsClusterSpec `json:"cluster"`
+	Cluster KopsClusterSpec `json:"cluster" yaml:"cluster"`
 
-	InstanceGroups []InstanceGroupSpec `json:"instanceGroups"`
+	InstanceGroups []InstanceGroupSpec `json:"instanceGroups" yaml:"instanceGroups"`
 
-	RollingUpdateOpts RollingUpdateOptsSpec `json:"rollingUpdateOpts,omitempty"`
+	RollingUpdateOpts RollingUpdateOptsSpec `json:"rollingUpdateOpts,omitempty" yaml:"rollingUpdateOpts,omitempty"`
 
-	Keypairs []KeypairSpec `json:"keypairs,omitempty"`
+	Keypairs []KeypairSpec `json:"keypairs,omitempty" yaml:"keypairs,omitempty"`
 
-	Secrets []SecretSpec `json:"secrets,omitempty"`
+	Secrets []SecretSpec `json:"secrets,omitempty" yaml:"secrets,omitempty"`
 
 	// Cluster is the spec provided for the kops api ClusterSpec; ref:
 	// https://pkg.go.dev/k8s.io/kops@v1.29.0/pkg/apis/kops#ClusterSpec
@@ -48,29 +48,29 @@ type ClusterParameters struct {
 
 type RollingUpdateOptsSpec struct {
 	// +kubebuilder:default=true
-	Enabled *bool `json:"enabled,omitempty"`
+	Enabled *bool `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	// +kubebuilder:default="15s"
-	BastionInterval *string `json:"bastionInterval,omitempty"`
+	BastionInterval *string `json:"bastionInterval,omitempty" yaml:"bastionInterval,omitempty"`
 	// +kubebuilder:default=false
-	CloudOnly *bool `json:"cloudOnly,omitempty"`
+	CloudOnly *bool `json:"cloudOnly,omitempty" yaml:"cloudOnly,omitempty"`
 	// +kubebuilder:default="15s"
-	ControlPlaneInterval *string `json:"controlPlaneInterval,omitempty"`
+	ControlPlaneInterval *string `json:"controlPlaneInterval,omitempty" yaml:"controlPlaneInterval,omitempty"`
 	// +kubebuilder:default="15m0s"
-	DrainTimeout *string `json:"drainTimeout,omitempty"`
+	DrainTimeout *string `json:"drainTimeout,omitempty" yaml:"drainTimeout,omitempty"`
 	// +kubebuilder:default=true
-	FailOnDrainError *bool `json:"failOnDrainError,omitempty"`
+	FailOnDrainError *bool `json:"failOnDrainError,omitempty" yaml:"failOnDrainError,omitempty"`
 	// +kubebuilder:default=true
-	FailOnValidateError *bool `json:"failOnValidateError,omitempty"`
+	FailOnValidateError *bool `json:"failOnValidateError,omitempty" yaml:"failOnValidateError,omitempty"`
 	// +kubebuilder:default=false
-	Force *bool `json:"force,omitempty"`
+	Force *bool `json:"force,omitempty" yaml:"force,omitempty"`
 	// +kubebuilder:default="15s"
-	NodeInterval *string `json:"nodeInterval,omitempty"`
+	NodeInterval *string `json:"nodeInterval,omitempty" yaml:"nodeInterval,omitempty"`
 	// +kubebuilder:default="15s"
-	PostDrainDelay *string `json:"postDrainDelay,omitempty"`
+	PostDrainDelay *string `json:"postDrainDelay,omitempty" yaml:"postDrainDelay,omitempty"`
 	// +kubebuilder:default=2
-	ValidateCount *int32 `json:"validateCount,omitempty"`
+	ValidateCount *int32 `json:"validateCount,omitempty" yaml:"validateCount,omitempty"`
 	// +kubebuilder:default="15m0s"
-	ValidationTimeout *string `json:"validationTimeout,omitempty"`
+	ValidationTimeout *string `json:"validationTimeout,omitempty" yaml:"validationTimeout,omitempty"`
 }
 
 // *****
@@ -81,16 +81,18 @@ type RollingUpdateOptsSpec struct {
 // for options description; ref: https://github.com/kubernetes/kops/blob/master/docs/cluster_spec.md
 // for code gen; ref: https://book.kubebuilder.io/reference/markers
 type KopsClusterSpec struct {
-	Assets             AssetsSpec             `yaml:"assets,omitempty" json:"assets,omitempty"`
-	Containerd         ContainerdConfigSpec   `yaml:"containerd,omitempty" json:"containerd,omitempty"`
-	AdditionalPolicies AdditionalPoliciesSpec `yaml:"additionalPolicies,omitempty" json:"additionalPolicies,omitempty"`
-	API                APISpec                `yaml:"api,omitempty" json:"api,omitempty"`
-	Authorization      *AuthorizationSpec     `yaml:"authorization,omitempty" json:"authorization,omitempty"`
-	Channel            string                 `yaml:"channel,omitempty" json:"channel,omitempty"`
-	CloudLabels        map[string]string      `yaml:"cloudLabels,omitempty" json:"cloudLabels,omitempty"`
-	CloudProvider      string                 `yaml:"cloudProvider,omitempty" json:"cloudProvider,omitempty"`
-	ClusterAutoscaler  ClusterAutoscalerSpec  `yaml:"clusterAutoscaler,omitempty" json:"clusterAutoscaler,omitempty"`
-	ConfigBase         string                 `yaml:"configBase,omitempty" json:"configBase,omitempty"`
+	Assets                 AssetsSpec                 `yaml:"assets,omitempty" json:"assets,omitempty"`
+	Containerd             ContainerdConfigSpec       `yaml:"containerd,omitempty" json:"containerd,omitempty"`
+	AdditionalPolicies     AdditionalPoliciesSpec     `yaml:"additionalPolicies,omitempty" json:"additionalPolicies,omitempty"`
+	API                    APISpec                    `yaml:"api,omitempty" json:"api,omitempty"`
+	Authorization          *AuthorizationSpec         `yaml:"authorization,omitempty" json:"authorization,omitempty"`
+	Channel                string                     `yaml:"channel,omitempty" json:"channel,omitempty"`
+	CloudLabels            map[string]string          `yaml:"cloudLabels,omitempty" json:"cloudLabels,omitempty"`
+	CloudProvider          string                     `yaml:"cloudProvider,omitempty" json:"cloudProvider,omitempty"`
+	ClusterAutoscaler      ClusterAutoscalerSpec      `yaml:"clusterAutoscaler,omitempty" json:"clusterAutoscaler,omitempty"`
+	NodeProblemDetector    NodeProblemDetectorSpec    `yaml:"nodeProblemDetector,omitempty" json:"nodeProblemDetector,omitempty"`
+	NodeTerminationHandler NodeTerminationHandlerSpec `yaml:"nodeTerminationHandler,omitempty" json:"nodeTerminationHandler,omitempty"`
+	ConfigBase             string                     `yaml:"configBase,omitempty" json:"configBase,omitempty"`
 	// +kubebuilder:validation:Enum=containerd
 	ContainerRuntime string             `yaml:"containerRuntime,omitempty" json:"containerRuntime,omitempty"`
 	EncryptionConfig bool               `yaml:"encryptionConfig,omitempty" json:"encryptionConfig,omitempty"`
@@ -186,6 +188,111 @@ type AuthorizationStructSpec map[string]string
 
 type ClusterAutoscalerSpec struct {
 	Enabled bool `yaml:"enabled" json:"enabled"`
+}
+
+// NodeProblemDetectorSpec
+// for options description; ref: https://github.com/kubernetes/kops/blob/69baa44474e7dc7b8f238adeacc349c22070fca9/pkg/apis/kops/componentconfig.go#L1068
+type NodeProblemDetectorSpec struct {
+	// Enabled enables the NodeProblemDetector.
+	// +kubebuilder:default=false
+	Enabled *bool `yaml:"enabled,omitempty" json:"enabled,omitempty"`
+
+	// Image is the NodeProblemDetector container image used.
+	Image *string `yaml:"image,omitempty" json:"image,omitempty"`
+
+	// MemoryRequest of NodeProblemDetector container.
+	// +kubebuilder:default="80Mi"
+	MemoryRequest *string `yaml:"memoryRequest,omitempty" json:"memoryRequest,omitempty"`
+
+	// CPURequest of NodeProblemDetector container.
+	// +kubebuilder:default="10m"
+	CPURequest *string `yaml:"cpuRequest,omitempty" json:"cpuRequest,omitempty"`
+
+	// MemoryLimit of NodeProblemDetector container.
+	// +kubebuilder:default="80Mi"
+	MemoryLimit *string `yaml:"memoryLimit,omitempty" json:"memoryLimit,omitempty"`
+
+	// CPULimit of NodeProblemDetector container.
+	CPULimit *string `yaml:"cpuLimit,omitempty" json:"cpuLimit,omitempty"`
+}
+
+// NodeTerminationHandlerSpec
+// for options description; ref: https://github.com/kubernetes/kops/blob/69baa44474e7dc7b8f238adeacc349c22070fca9/pkg/apis/kops/componentconfig.go#L995
+type NodeTerminationHandlerSpec struct {
+	// DeleteSQSMsgIfNodeNotFound makes node termination handler delete the SQS Message from the SQS Queue if the targeted node is not found.
+	// Only used in Queue Processor mode.
+	// +kubebuilder:default=false
+	DeleteSQSMsgIfNodeNotFound *bool `yaml:"deleteSQSMsgIfNodeNotFound,omitempty" json:"deleteSQSMsgIfNodeNotFound,omitempty"`
+
+	// Enabled enables the node termination handler.
+	// +kubebuilder:default=false
+	Enabled *bool `yaml:"enabled,omitempty" json:"enabled,omitempty"`
+
+	// EnableSpotInterruptionDraining makes node termination handler drain nodes when spot interruption termination notice is received.
+	// Cannot be disabled in queue-processor mode.
+	// +kubebuilder:default=true
+	EnableSpotInterruptionDraining *bool `yaml:"enableSpotInterruptionDraining,omitempty" json:"enableSpotInterruptionDraining,omitempty"`
+
+	// EnableScheduledEventDraining makes node termination handler drain nodes before the maintenance window starts for an EC2 instance scheduled event.
+	// Cannot be disabled in queue-processor mode.
+	// +kubebuilder:default=true
+	EnableScheduledEventDraining *bool `yaml:"enableScheduledEventDraining,omitempty" json:"enableScheduledEventDraining,omitempty"`
+
+	// EnableRebalanceMonitoring makes node termination handler cordon nodes when the rebalance recommendation notice is received.
+	// In queue-processor mode, cannot be enabled without rebalance draining.
+	// +kubebuilder:default=false
+	EnableRebalanceMonitoring *bool `yaml:"enableRebalanceMonitoring,omitempty" json:"enableRebalanceMonitoring,omitempty"`
+
+	// EnableRebalanceDraining makes node termination handler drain nodes when the rebalance recommendation notice is received.
+	// +kubebuilder:default=false
+	EnableRebalanceDraining *bool `yaml:"enableRebalanceDraining,omitempty" json:"enableRebalanceDraining,omitempty"`
+
+	// EnablePrometheusMetrics enables the "/metrics" endpoint.
+	// +kubebuilder:default=false
+	EnablePrometheusMetrics *bool `yaml:"prometheusEnable,omitempty" json:"prometheusEnable,omitempty"`
+
+	// EnableSQSTerminationDraining enables queue-processor mode which drains nodes when an SQS termination event is received.
+	// +kubebuilder:default=true
+	EnableSQSTerminationDraining *bool `yaml:"enableSQSTerminationDraining,omitempty" json:"enableSQSTerminationDraining,omitempty"`
+
+	// ExcludeFromLoadBalancers makes node termination handler will mark for exclusion from load balancers before node are cordoned.
+	// +kubebuilder:default=true
+	ExcludeFromLoadBalancers *bool `yaml:"excludeFromLoadBalancers,omitempty" json:"excludeFromLoadBalancers,omitempty"`
+
+	// ManagedASGTag is the tag used to determine which nodes NTH can take action on
+	// This field has kept its name even though it now maps to the --managed-tag flag due to keeping the API stable.
+	// Node termination handler does no longer check the ASG for this tag, but the actual EC2 instances.
+	// +kubebuilder:default="aws-node-termination-handler/managed"
+	ManagedASGTag *string `yaml:"managedASGTag,omitempty" json:"managedASGTag,omitempty"`
+
+	// PodTerminationGracePeriod is the time in seconds given to each pod to terminate gracefully.
+	// If negative, the default value specified in the pod will be used, which defaults to 30 seconds if not specified for the pod.
+	// +kubebuilder:default=-1
+	PodTerminationGracePeriod *int32 `yaml:"podTerminationGracePeriod,omitempty" json:"podTerminationGracePeriod,omitempty"`
+
+	// TaintNode makes node termination handler taint nodes when an interruption event occurs.
+	// +kubebuilder:default=false
+	TaintNode *bool `yaml:"taintNode,omitempty" json:"taintNode,omitempty"`
+
+	// MemoryLimit of NodeTerminationHandler container.
+	MemoryLimit *string `yaml:"memoryLimit,omitempty" json:"memoryLimit,omitempty"`
+
+	// MemoryRequest of NodeTerminationHandler container.
+	// +kubebuilder:default="64Mi"
+	MemoryRequest *string `yaml:"memoryRequest,omitempty" json:"memoryRequest,omitempty"`
+
+	// CPURequest of NodeTerminationHandler container.
+	// +kubebuilder:default="50m"
+	CPURequest *string `yaml:"cpuRequest,omitempty" json:"cpuRequest,omitempty"`
+
+	// Version is the container image tag used.
+	Version *string `yaml:"version,omitempty" json:"version,omitempty"`
+
+	// Replaces the default webhook message template.
+	WebhookTemplate *string `yaml:"webhookTemplate,omitempty" json:"webhookTemplate,omitempty"`
+
+	// If specified, posts event data to URL upon instance interruption action.
+	WebhookURL *string `yaml:"webhookURL,omitempty" json:"webhookURL,omitempty"`
 }
 
 type DockerSpec struct {
@@ -286,8 +393,38 @@ type KubeProxySpec struct {
 	ProxyMode string `yaml:"proxyMode" json:"proxyMode"`
 }
 
+// KubeDNSSpec
+// for options description; ref: https://github.com/kubernetes/kops/blob/69baa44474e7dc7b8f238adeacc349c22070fca9/pkg/apis/kops/cluster.go#L568
 type KubeDNSSpec struct {
 	Provider string `yaml:"provider" json:"provider"`
+
+	// NodeLocalDNS specifies the configuration for the node-local-dns addon
+	NodeLocalDNS *NodeLocalDNSConfig `yaml:"nodeLocalDNS,omitempty" json:"nodeLocalDNS,omitempty"`
+}
+
+// NodeLocalDNSConfig
+// for options description; ref: https://github.com/kubernetes/kops/blob/69baa44474e7dc7b8f238adeacc349c22070fca9/pkg/apis/kops/cluster.go#L604
+type NodeLocalDNSConfig struct {
+	// Enabled activates the node-local-dns addon.
+	// +kubebuilder:default=false
+	Enabled *bool `yaml:"enabled,omitempty" json:"enabled,omitempty"`
+	// ExternalCoreFile is used to provide a complete NodeLocalDNS CoreFile by the user - ignores other provided flags which modify the CoreFile.
+	ExternalCoreFile string `yaml:"externalCoreFile,omitempty" json:"externalCoreFile,omitempty"`
+	// AdditionalConfig is used to provide additional config for node local dns by the user - it will include the original CoreFile made by kOps.
+	AdditionalConfig string `yaml:"additionalConfig,omitempty" json:"additionalConfig,omitempty"`
+	// Image overrides the default container image used for node-local-dns addon.
+	Image *string `yaml:"image,omitempty" json:"image,omitempty"`
+	// Local listen IP address. It can be any IP in the 169.254.20.0/16 space or any other IP address that can be guaranteed to not collide with any existing IP.
+	LocalIP string `yaml:"localIP,omitempty" json:"localIP,omitempty"`
+	// If enabled, nodelocal dns will use kubedns as a default upstream.
+	ForwardToKubeDNS *bool `yaml:"forwardToKubeDNS,omitempty" json:"forwardToKubeDNS,omitempty"`
+	// MemoryRequest specifies the memory requests of each node-local-dns container in the daemonset. Default 5Mi.
+	MemoryRequest *string `yaml:"memoryRequest,omitempty" json:"memoryRequest,omitempty"`
+	// CPURequest specifies the cpu requests of each node-local-dns container in the daemonset. Default 25m.
+	CPURequest *string `yaml:"cpuRequest,omitempty" json:"cpuRequest,omitempty"`
+	// PodAnnotations makes possible to add additional annotations to node-local-dns.
+	// Default: none
+	PodAnnotations map[string]string `yaml:"podAnnotations,omitempty" json:"podAnnotations,omitempty"`
 }
 
 type MetricsServerSpec struct {
@@ -456,20 +593,20 @@ type ClusterStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster,categories={crossplane,managed,kops}
 type Cluster struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+	metav1.TypeMeta   `yaml:",inline" json:",inline"`
+	metav1.ObjectMeta `yaml:"metadata,omitempty" json:"metadata,omitempty"`
 
-	Spec   ClusterSpec   `json:"spec"`
-	Status ClusterStatus `json:"status,omitempty"`
+	Spec   ClusterSpec   `yaml:"spec" json:"spec"`
+	Status ClusterStatus `yaml:"status,omitempty" json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 
 // ClusterList contains a list of Cluster
 type ClusterList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Cluster `json:"items"`
+	metav1.TypeMeta `yaml:",inline" json:",inline"`
+	metav1.ListMeta `yaml:"metadata,omitempty" json:"metadata,omitempty"`
+	Items           []Cluster `yaml:"items" json:"items"`
 }
 
 // Cluster type metadata.
